3339cd325d0a531cbb1d044a441831116ba221f4
sukeshperiyasamy <sukeeshsss200@gmail.com>
2024-03-11 01:52:37 +0530

    Day 2

---
+++ Logical.java
@@ -0,0 +1,21 @@
+class Logical
+{
+    public static void main(String args[])
+    {
+int a =10;
+int b = 10;
+
+
+int m=10;
+int n=1;
+
+// boolean result = a!=b && m!=n;
+// boolean result = a==b || m==n;
+
+boolean result = a == b  || m==n;
+System.out.println(!result);
+
+
+
+    }
+}
